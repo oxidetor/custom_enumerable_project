e2ed8703dab088c19c3385a17310f9d469d7e56c
oxidetor <prateek.girdhar@outlook.com>
2022-07-12 13:56:43 -0700

    Add initial solutions

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -1,5 +1,89 @@
 module Enumerable
   # Your code goes here
+
+  def my_each_with_index
+    index = 0
+    until index == size
+      yield(self[index], index)
+      index += 1
+    end
+    self
+  end
+
+  def my_select
+    matched_items = []
+    index = 0
+    until index == size
+      matched_items << self[index] if yield(self[index])
+      index += 1
+    end
+    matched_items
+  end
+
+  def my_all?
+    index = 0
+    until index == size
+      return false unless yield(self[index])
+
+      index += 1
+    end
+    true
+  end
+
+  def my_any?
+    index = 0
+    until index == size
+      return true if yield(self[index])
+
+      index += 1
+    end
+    false
+  end
+
+  def my_none?
+    index = 0
+    until index == size
+      return false if yield(self[index])
+
+      index += 1
+    end
+    true
+  end
+
+  def my_count
+    return size unless block_given?
+
+    count = 0
+    index = 0
+    until index == size
+      count += 1 if yield(self[index])
+
+      index += 1
+    end
+    count
+  end
+
+  def my_map(&function)
+    new_arr = []
+    index = 0
+    until index == size
+      new_arr << function.call(self[index])
+
+      index += 1
+    end
+    new_arr
+  end
+
+  def my_inject(initializer, &function)
+    accumulator = initializer
+    index = 0
+    until index == size
+      accumulator = function.call(accumulator, self[index])
+
+      index += 1
+    end
+    accumulator
+  end
 end
 
 # You will first have to define my_each
@@ -8,4 +92,12 @@ end
 # to this method
 class Array
   # Define my_each here
+  def my_each
+    index = 0
+    until index == size
+      yield(self[index])
+      index += 1
+    end
+    self
+  end
 end
